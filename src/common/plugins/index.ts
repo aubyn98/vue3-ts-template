cdd550ad76ecc0fb27046da08bd884561b88bb40
aubyn <aubyn@vip.qq.com>
2021-07-20 13:04:54 +0800

    change babel

--- src/common/plugins/index.ts
+++ src/common/plugins/index.ts
@@ -4,7 +4,7 @@ import utils from 'utils'
 import comp from 'comp'
 export default function (app: App) {
   const plugins = require.context('./', true, /\.(js|ts)$/)
-  plugins.keys().forEach((pluginPath) => {
+  plugins.keys().forEach(pluginPath => {
     // const pluginName = pluginPath.match(/.*\/(.*)\.(js|ts)$/)![1]
     if (pluginPath !== './index.ts') {
       plugins(pluginPath).default(app)
